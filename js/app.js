e5424503d5c9ec1a1c59dcf1bc6ac55f745a379b
SandyPanG <soleibiza@hotmail.com>
2018-10-25 00:19:59 -0500

    convert input to array

--- js/app.js
+++ js/app.js
@@ -4,11 +4,19 @@ do {
   credCard = prompt("Ingresa tu numero de Tarjeta de Credito a 16 digitos \n sin espacios."); // Aquí se da al usuario las instrucciones para que ingrese su número de T.C. 
 }
 
-	while( !credCard || !/^[0-9]*$/.test(credCard));
+	while( !credCard || !/^[0-9]*$/.test(credCard));//prueba que no se pueda dejar sin ingresar datos o que no pase si la información no contiene sólo números
+  isValidCard(credCard);
+  document.getElementById("numero").innerHTML = credCard
 
-// function isValidCard (num) {
-//   var array = [];
-//   console.log(array);
+
+function isValidCard (card) {
+  var array = (credCard.split(""));
+	var reverseArray = [];
+	var posPair = [];
+	var sumPair = 0;
+	var newMultArray = [];
   
-//     };
+  console.log(array);
+  
+    };
 
