0aaa9bd60d9887a15f6bf93137c0bc67e0cc17d6
SandyPanG <soleibiza@hotmail.com>
2018-10-25 00:46:17 -0500

    trying reverse array

--- js/app.js
+++ js/app.js
@@ -8,15 +8,17 @@ do {
   isValidCard(credCard);
   document.getElementById("numero").innerHTML = credCard
 
-
+/**************Función que prueba si el número es válido************/ 
 function isValidCard (card) {
-  var array = (credCard.split(""));
-	var reverseArray = [];
+  var array = (credCard.split(""));//convirtiendo en array el número ingresado
+	var reverseArray = array.reverse();//colocando al revés el array
 	var posPair = [];
 	var sumPair = 0;
 	var newMultArray = [];
   
   console.log(array);
+  console.log(reverseArray);
+  
   
     };
 
